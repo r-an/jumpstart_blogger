7e29aa46c6f8dc376351caebcf46d20593ca58ed
Main Account <mrxmemo@yahoo.com>
2015-09-08 22:11:32 -0700

    finished blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,8 @@ class ArticlesController < ApplicationController
    end
    def show
        @article = Article.find(params[:id])
+       @comment = Comment.new
+       @comment.article_id = @article.id
    end
    def new
        @article = Article.new
@@ -20,7 +22,7 @@ class ArticlesController < ApplicationController
    def destroy
        @article = Article.find(params[:id])
        @article.destroy
-       redirect_to article_path(@article)
+       redirect_to articles_path
    end
    def update
        @article = Article.find(params[:id])
